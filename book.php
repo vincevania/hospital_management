71b3d5bfdefddf3b33fbd39f19e16a43e9a5bd01
Ritesh Biswakarma <119935389+heymrgoodboy@users.noreply.github.com>
2024-04-19 01:13:34 +0530

    sucessfulyy  connected databsse for appointments  and feteched in administrative state and in the following state and created appointment booking that need to be changed

--- book.php
+++ book.php
@@ -1,40 +1,36 @@
 <?php
-if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $reg_no = $_POST['reg_no'];
-    $age = $_POST['age'];
-    $email = $_POST['email'];
-    $phone_number = $_POST['phone_number'];
-    $date = $_POST['date'];
-    $time = $_POST['time'];
-    $issue = $_POST['issue'];
-    $doctorname = $_POST['doctorname'];
-        
-    // Create a database connection
+if (isset($_POST['submit'])) {
+
+    // Establish connection to the database
     $conn = new mysqli("localhost", "root", "", "hospital");
 
-    // Check if the connection is successful
+    // Check connection
     if($conn->connect_error){
         die("Connection failed: ". $conn->connect_error);
-    }
-
-    // SQL query to insert data into the 'appointment' table
-    $query = "INSERT INTO appointment 
-    (name , reg_no, age, email, phone_number, date, time, issue, doctorname)
-    VALUES 
-    ('$name', '$reg_no', '$age', '$email', '$phone_number', '$date', '$time', '$issue', '$doctorname')";
-
-    // Execute the query
-    if($conn->query($query) === TRUE){
-        echo "Registration successful";
-        header("Location: index.html");
     } else {
-        echo "Error: " . $query . "<br>" . $conn->error;
-    }
+        echo "Connection successful" ."<br>";
 
-    // Close the database connection
-    $conn->close();
-} else {
-    echo "Form not submitted";
+        // Retrieve values from the form
+        $name = $_POST['name'];
+        $reg_no = $_POST['reg_no'];
+        $age = $_POST['age'];
+        $email = $_POST['email'];
+        $phone_number = $_POST['phone_number'];
+        $date = $_POST['date'];
+        $time = $_POST['time'];
+        $issue = $_POST['issue'];
+        $doctorname = $_POST['doctorname'];
+
+        // SQL query to insert data into the 'appointment' table
+        $query = "INSERT INTO appointment (name, reg_no, age, email, phone_number, date, time, issue, doctorname)
+                  VALUES ('$name', '$reg_no', '$age', '$email', '$phone_number', '$date', '$time', '$issue', '$doctorname')";
+
+        // Execute query
+        if($conn->query($query) === TRUE){
+            echo "Registration successful";
+        } else {
+            echo "Error: " . $query . "<br>" . $conn->error;
+        }
+    }
 }
 ?>
--- registration.php
+++ registration.php
@@ -1,3 +1,19 @@
+<?php
+// Connect to the database
+$conn = new mysqli("localhost", "root", "", "hospital");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query to fetch registration details from the user table
+$sql = "SELECT * FROM appointment";
+$result = $conn->query($sql);
+
+// Close the database connection
+$conn->close();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +21,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/admin.css">
+    <link rel="stylesheet" href="../project/css/admin.css">
     <title>Admin Panel</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/solid.min.css" integrity="sha512-Hp+WwK4QdKZk9/W0ViDvLunYjFrGJmNDt6sCflZNkjgvNq9mY+0tMbd6tWMiAlcf1OQyqL4gn2rYp7UsfssZPA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -15,13 +31,13 @@
 <body>
     <div class="side-menu">
         <div class="brand-name">
-            <img class="logo" src="../../images/logo-removebg-preview.png" alt="">
+            <img class="logo" src="../project/images/logo-removebg-preview.png" alt="">
         </div>
         <ul>
            <a href="admin.html">
             <li><i class="fa-solid fa-bars"></i>&nbsp; <span >Dashboard</span> </li>
            </a>
-            <a href="registration.html">
+            <a href="registration.php">
                 <li><i class="fa-solid fa-file-pen"></i>&nbsp;<span>Registrations</span> </li>
             </a>
             <a href="addPatient.html">
@@ -29,53 +45,62 @@
             </a>
             <a href="history.html">
                 <li><i class="fa-solid fa-notes-medical"></i>&nbsp;<span>History</span> </li>
+            </a> <a href="user.html">
+                <li><i class="fa-solid fa-notes-medical"></i>&nbsp;<span>Users</span> </li>
             </a>
+
         </ul>
     </div>
     <div class="container">
-    <?php
-             $conn = new mysqli("localhost", "root", "", "hospital");
-             $query = "SELECT * FROM user";
-             $result = $conn->query($query);               
-    ?>
+
         <div class="content">
 
             <div class="content-2">
                 <div class="recent-payments">
                     <div class="title">
-                        <h2>All Registrations</h2>
+                        <h2>Appointments Booking</h2>
 
                     </div>
                     <table>
-                        <thead>
-                             <th>SN</th> 
+                        <tr>
+                            <th>ID</th>
                             <th>Name</th>
-                            <th>Registration Number</th> 
+                            <th>Registration Number</th>
+                            <th>Age</th>
                             <th>Email</th>
                             <th>Mobile</th>
+                            <th>Date</th>
                             <th>Time</th>
                             <th>Medical Issue</th>
                             <th>Doctor Name</th>
-                            <th>Actions</th> 
-                        </thead>
-                        <tbody>
-<?php
+                            <th>Actions</th>
+                        </tr>
+                       
+                        <?php
     if($result->num_rows > 0){
         while($row = $result->fetch_assoc()){
-            echo "<tr>";
+           echo "<tr>";
+            echo "<td>".$row['id']."</td>";
             echo "<td>".$row['name']."</td>";
-            echo "<td>".$row['regno']."</td>";
+            echo "<td>".$row['reg_no']."</td>";
+            echo "<td>".$row['age']."</td>";
             echo "<td>".$row['email']."</td>";
-            echo "<td>".$row['mobile']."</td>";
+            echo "<td>".$row['phone_number']."</td>";
+            echo "<td>".$row['date']."</td>";
             echo "<td>".$row['time']."</td>";
             echo "<td>".$row['issue']."</td>";
-            echo "<td>".$row['doc_name']."</td>";
-            echo "<td>".$row['action']."</td>";
+            echo "<td>".$row['doctorname']."</td>";
+           
             echo "</tr>";
+         
+      
         }
     }
 ?>
-                        </tbody>
+                            
+                        </tr>
+   
+                        
                     </table>
                 </div>
 
